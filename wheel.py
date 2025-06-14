707a128a4ca434464bfce6e3c760bc99a4e7ee29
99marko <a.hamdy2111@gmail.com>
2025-06-14 20:18:19 +0800

    Update

--- wheel.py
+++ wheel.py
@@ -15,30 +15,28 @@ interval = input("Interval in degrees=")
 scale = int(input("Enter the scale factor of the drawing (it's too smol): "))
 step = (eval(interval)/360) * (eval(xEnding)-eval(xStarting))
 screen = pygame.display.set_mode((800, 800))
-screen.fill((0, 0, 0))
 center = (400, 400)
-i = 1
 xF = eval(xStarting)
-for angle in range(0, 360, eval(interval)):
-    angle = math.radians(angle)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    result = f(xF)
-    dot_x = 400 + (math.cos(angle)*eval(axleH) - math.cos(angle)*result) * scale
-    dot_y = 400 + (math.sin(angle)*eval(axleH) - math.sin(angle)*result) * scale
-    pygame.draw.circle(screen, (255, 0, 255), (dot_x, dot_y), radius)
-    pygame.draw.circle(screen, (255, 255, 255), (400, 400), 5)
-    pygame.display.flip()
-    clock.tick(60)
+while True:
+    screen.fill((0, 0, 0))
+    for angle in range(0, 360, eval(interval)):
+        angle = math.radians(angle)
+        interval_rad = math.radians(eval(interval))
+        angle_next = angle + interval_rad
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        result = f(xF)
+        dot_x = 400 + (math.cos(angle)*eval(axleH) - math.cos(angle)*result) * scale
+        dot_y = 400 + (math.sin(angle)*eval(axleH) - math.sin(angle)*result) * scale
+        dot_x_next = 400 + (math.cos(angle_next)*eval(axleH) - math.cos(angle_next)*(f(xF+step))) * scale
+        dot_y_next = 400 + (math.sin(angle_next)*eval(axleH) - math.sin(angle_next)*(f(xF+step))) * scale
+        pygame.draw.circle(screen, (255, 255, 255), (400, 400), 5)
+        pygame.draw.circle(screen, (255, 0, 255), (dot_x, dot_y), radius)
+        pygame.display.flip()
+        clock.tick(144)
+        time.sleep(0.05)
+        xF += step
     time.sleep(0.05)
-    xF += step
-waiting = True
-while waiting:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        if event.type == pygame.KEYDOWN:
-            waiting = False
+    continue
